dc690ef92fae1c654704cf7ad511891689f8e361
unknown <nipcts3@gmail.com>
2024-12-19 13:05:23 +0200

    init

---
+++ src/main/java/com/nipcts/ihirwe/photoz_clone/PhotozController.java
@@ -0,0 +1,36 @@
+package com.nipcts.ihirwe.photoz_clone;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
+
+@RestController
+public class PhotozController {
+    private Map<String, Photo> db = new HashMap<>() {{
+        put("1", new Photo("1", "hello.jpg"));
+    }};
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world ";
+    }
+    @GetMapping("/photoz")
+    public Collection<Photo> get() {
+        return db.values();
+    }
+    @GetMapping("/photoz/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+    @DeleteMapping("/photoz/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = db.remove(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+}
