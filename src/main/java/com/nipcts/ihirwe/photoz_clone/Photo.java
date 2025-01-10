ca5b1b47086433375ef9d16f20e7cfa16388fa2b
unknown <nipcts3@gmail.com>
2025-01-10 08:10:20 +0200

    upload

--- src/main/java/com/nipcts/ihirwe/photoz_clone/Photo.java
+++ src/main/java/com/nipcts/ihirwe/photoz_clone/Photo.java
@@ -4,13 +4,25 @@ import jakarta.validation.constraints.NotEmpty;
 
 public class Photo {
     private String id;
+
+
     @NotEmpty
     private String fileName;
+    private byte[] data;
+    public Photo() {}
 
     public Photo(String id, String fileName) {
         this.id = id;
         this.fileName = fileName;
     }
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
     public String getId() {
         return id;
     }
--- src/main/java/com/nipcts/ihirwe/photoz_clone/PhotozController.java
+++ src/main/java/com/nipcts/ihirwe/photoz_clone/PhotozController.java
@@ -1,12 +1,16 @@
 package com.nipcts.ihirwe.photoz_clone;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 public class PhotozController {
     private Map<String, Photo> db = new HashMap<>() {{
         put("1", new Photo("1", "hello.jpg"));
@@ -30,9 +34,14 @@ public class PhotozController {
         Photo photo = db.remove(id);
         if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
+    //created the photo by creating the photo id using a mark annotation @RequestBody to change the photo into the Photo object
+    // used the validation to prevent empty files
     @PostMapping("/photoz/")
-        public Photo create(@RequestBody Photo photo) {
+        public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = new Photo();
         photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(file.getOriginalFilename());
+        photo.setData(file.getBytes());
           db.put(photo.getId(), photo);
           return photo;
         }
